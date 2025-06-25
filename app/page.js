98ea28455859642fc44f19c7bf2fac8600ed670c
Shaharia hasan <hshaharia064@gmail.com>
2025-06-25 15:48:35 +0530

    sliding text animation using next js and gsap

--- app/page.js
+++ app/page.js
@@ -1,103 +1,108 @@
-import Image from "next/image";
+'use client'
+import { useEffect } from "react";
+import gsap from "gsap/all";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleWheel)
+
+    function handleWheel(events) {
+      if (events.deltaY >= 0) {
+        console.log('scrolling down')
+
+         gsap.to('.mark', {
+      transform: 'translatex(200%)',
+      duration: 2,
+      // delay : 2
+      ease: 'none',
+      // repeat: -1
+    })
+
+    gsap.to('.mark img', {
+      rotate: 180,
+      duration: 1,
+      // delay : 2,
+
+    })
+      } else {
+        console.log('scrolling up');
+         gsap.to('.mark', {
+      transform: 'translatex(0%)',
+      duration: 2,
+      // delay : 2
+      ease: 'none',
+      // repeat: -1
+    })
+
+    gsap.to('.mark img', {
+      rotate: 0,
+      duration: 1,
+      // delay : 2,
+      // repeat : -1
+
+    })
+
+      }
+    }
+
+
+   
+
+
+
+    return () => {
+      window.removeEventListener('wheel')
+    }
+
+
+
+
+  }, [])
+
+  return (
+    <>
+      <div id="page1" className="h-[100vh] w-full overflow-x-hidden">hello</div>
+
+
+      <div id="page2" className="h-[100vh] w-full ">hello
+        <div id="move" className="bg-lime-600 py-10 px-5 flex overflow-hidden ">
+          <div className="mark flex shrink-0 gap-10 items-center  text-gray-950 ">
+            <h1 className="text-7xl font-semibold">Nothing Like Anything</h1>
+            <img
+              className="size-20"
+              src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="img" />
+          </div>
+          <div className="mark flex shrink-0 gap-10 items-center text-gray-950">
+            <h1 className="text-7xl font-semibold">Nothing Like Anything</h1>
+            <img
+              className="size-20"
+              src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="img" />
+          </div>
+          <div className="mark flex shrink-0 gap-10 items-center text-gray-950">
+            <h1 className="text-7xl font-semibold">Nothing Like Anything</h1>
+            <img
+              className="size-20"
+              src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="img" />
+          </div>
+          <div className="mark flex shrink-0 gap-10 items-center text-gray-950">
+            <h1 className="text-7xl font-semibold">Nothing Like Anything</h1>
+            <img
+              className="size-20"
+              src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="img" />
+          </div>
+          <div className="mark flex shrink-0 gap-10 items-center text-gray-950">
+            <h1 className="text-7xl font-semibold">Nothing Like Anything</h1>
+            <img
+              className="size-20"
+              src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="img" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+
+      <div id="page3" className="h-[100vh] w-full overflow-x-hidden">hello</div>
+
+    </>
   );
 }
